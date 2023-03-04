af824ec4964fc32ba82072de5d6c7ed1ccfe27a7
Swapnil Akulwar <91781600+swapya97@users.noreply.github.com>
2023-03-04 14:13:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Name from './components/ExpamleOfComponent';
+function App() {
+  return (
+<Name/>
+  );
+}
+
+export default App;
---
+++ src/components/ExpamleOfComponent.js
@@ -0,0 +1,9 @@
+const Name =_=>{
+return(
+    <div>
+        <h1>hello iam here..</h1>
+        <button>click me </button>
+    </div>
+)
+}
+export default Name;
